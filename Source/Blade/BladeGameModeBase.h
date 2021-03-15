0ab58ec051cd9537df0854187ad28e31e69b7fec
Yan Jun Li <yanjunlivr911@gmail.com>
2021-03-15 15:52:21 +0800

    Add default UE4 project

---
+++ Source/Blade/BladeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BladeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLADE_API ABladeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
