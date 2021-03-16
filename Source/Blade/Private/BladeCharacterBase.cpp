7a162fe2cc75ace695d2b82357e669c9d0d81806
Yan Jun Li <yanjunlivr911@gmail.com>
2021-03-16 09:54:51 +0800

    Handle damage and health changed

--- Source/Blade/Private/BladeCharacterBase.cpp
+++ Source/Blade/Private/BladeCharacterBase.cpp
@@ -1,4 +1,10 @@
 #include "BladeCharacterBase.h"
+#include "BladeAttributeSet.h"
+
+ABladeCharacterBase::ABladeCharacterBase()
+{
+	AttributeSet = CreateDefaultSubobject<UBladeAttributeSet>(TEXT("AttributeSet"));
+}
 
 void ABladeCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
@@ -8,6 +14,36 @@ void ABladeCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInput
 	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ABladeCharacterBase::MoveRight);
 	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APawn::AddControllerPitchInput);
 	PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &APawn::AddControllerYawInput);
+}
+
+void ABladeCharacterBase::HandleDamage(float DamageAmount, const FHitResult& HitInfo, const FGameplayTagContainer& DamageTags, ABladeCharacterBase* InstigatorCharacter, AActor* DamageCauser)
+{
+	OnDamaged(DamageAmount, HitInfo, DamageTags, InstigatorCharacter, DamageCauser);
+}
+
+void ABladeCharacterBase::HandleHealthChanged(float DeltaValue, const FGameplayTagContainer& EventTags)
+{
+	OnHealthChanged(DeltaValue, EventTags);
+}
+
+float ABladeCharacterBase::GetHealth() const
+{
+	if (IsValid(AttributeSet))
+	{
+		return AttributeSet->GetHealth();
+	}
+
+	return 0.0f;
+}
+
+float ABladeCharacterBase::GetMaxHealth() const
+{
+	if (IsValid(AttributeSet))
+	{
+		return AttributeSet->GetMaxHealth();
+	}
+
+	return 0.0f;
 }
 
 void ABladeCharacterBase::MoveForward(float AxisValue)
--- Source/Blade/Public/BladeCharacterBase.h
+++ Source/Blade/Public/BladeCharacterBase.h
@@ -2,7 +2,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "GameplayTagContainer.h"
 #include "BladeCharacterBase.generated.h"
+
+class UBladeAttributeSet;
 
 UCLASS()
 class BLADE_API ABladeCharacterBase : public ACharacter
@@ -10,11 +13,28 @@ class BLADE_API ABladeCharacterBase : public ACharacter
 	GENERATED_BODY()
 
 public:
-	ABladeCharacterBase() = default;
+	ABladeCharacterBase();
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void HandleDamage(float DamageAmount, const FHitResult& HitInfo, const struct FGameplayTagContainer& DamageTags, ABladeCharacterBase* InstigatorCharacter, AActor* DamageCauser);
+	virtual void HandleHealthChanged(float DeltaValue, const struct FGameplayTagContainer& EventTags);
+
+	UFUNCTION(BlueprintCallable)
+	virtual float GetHealth() const;
+
+	UFUNCTION(BlueprintCallable)
+	virtual float GetMaxHealth() const;
 
 protected:
 	void MoveForward(float AxisValue);
 	void MoveRight(float AxisValue);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnDamaged(float DamageAmount, const FHitResult& HitInfo, const struct FGameplayTagContainer& DamageTags, ABladeCharacterBase* InstigatorCharacter, AActor* DamageCauser);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnHealthChanged(float DeltaValue, const struct FGameplayTagContainer& EventTags);
+
+	UPROPERTY()
+	UBladeAttributeSet* AttributeSet;
 };
