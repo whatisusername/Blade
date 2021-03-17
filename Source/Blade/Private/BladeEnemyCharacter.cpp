6a0d80b0c9db24c3dd1ea1f99bf62b8b0cb35610
Yan Jun Li <yanjunlivr911@gmail.com>
2021-03-17 11:36:06 +0800

    Add native user widget

--- Source/Blade/Private/BladeEnemyCharacter.cpp
+++ Source/Blade/Private/BladeEnemyCharacter.cpp
@@ -1,7 +1,18 @@
 #include "BladeEnemyCharacter.h"
+#include "BladeHealthBarWidgetBase.h"
 #include "Components/WidgetComponent.h"
 
 ABladeEnemyCharacter::ABladeEnemyCharacter()
 {
 	WidgetComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("HPWidget"));
+}
+
+void ABladeEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	WidgetComponent->SetRelativeLocation(FVector(0, 0, 180));
+	WidgetComponent->SetWidgetSpace(EWidgetSpace::Screen);
+	WidgetComponent->SetDrawSize(FVector2D(100, 16));
+	WidgetComponent->SetWidgetClass(WidgetClass.Get());
 }
