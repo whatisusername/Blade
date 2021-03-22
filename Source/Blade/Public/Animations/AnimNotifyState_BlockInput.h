61cfac8b8ca7f125f1886447e3d72df394548855
Yan Jun Li <yanjunlivr911@gmail.com>
2021-03-22 15:59:31 +0800

    Rename block player input anim notify state

--- Source/Blade/Public/Animations/AnimNotifyState_BlockInput.h
+++ Source/Blade/Public/Animations/AnimNotifyState_BlockInput.h
@@ -2,10 +2,10 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimNotifies/AnimNotifyState.h"
-#include "BladeAnimNotifyState_BlockInput.generated.h"
+#include "AnimNotifyState_BlockInput.generated.h"
 
 UCLASS()
-class BLADE_API UBladeAnimNotifyState_BlockInput : public UAnimNotifyState
+class BLADE_API UAnimNotifyState_BlockInput : public UAnimNotifyState
 {
 	GENERATED_BODY()
 
